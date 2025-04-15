9b67882c27ebb3bf8dfc4fc9ccd4d8305a7c7937
kevindev10 <kevindev1@outlook.com>
2025-04-15 14:30:30 +0300

    Updated docker-compose file, Application works

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
@@ -9,6 +10,8 @@ const productRoute = require('./routes/api/productRoute');
 // Connecting to the Database
 let mongodb_url = 'mongodb://localhost/';
 let dbName = 'yolomy';
+
+
 
 // define a url to connect to the database
 const MONGODB_URI = process.env.MONGODB_URI || mongodb_url + dbName
